589841abb2a33bc927fbb865fb69bf218bbb6da2
JonathanMarkert <jonathan@markert.se>
2021-08-27 19:53:50 +0200

    added get by id

--- Routes/Fishes.js
+++ Routes/Fishes.js
@@ -9,12 +9,22 @@ const fishes =[
 
 router.get('/', (req, res) => {
     res.send(fishes);
-})
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    
+
+    res.send(fishes.find((fish)=> fish.id === id));
+
+});
+
 
 router.post('/', (req, res)=> {    
     const fish = req.body;
-    fishes.push({ id: uuidv4(),...fish })
-    res.send(`Fish of type: ${fish.type} added to database.`)
-})
+    fishes.push({ id: uuidv4(),...fish });
+    res.send(`Fish of type: ${fish.type} added to database.`);
+});
 
 export default router;
