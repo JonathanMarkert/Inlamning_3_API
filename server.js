c9cb296d6c34ee7b81c8c3868083c0b68b04949b
JonathanMarkert <jonathan@markert.se>
2021-08-29 17:10:31 +0200

    cleaned up server.js

--- server.js
+++ server.js
@@ -6,10 +6,4 @@ const PORT = 5000;
 
 app.use('/fishes', fishesRoutes )
 
-app.get('/', (req, res) => {
-    console.log('Test');
-
-    res.send('Hello from the homepage');
-})
-
 app.listen(PORT, () => console.log(`Server up and running on port: http://localhost:${PORT}`));
